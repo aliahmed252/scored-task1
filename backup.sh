406fd8988933cc72a975dd70ba7fcbac9d4ea52b
Ali ahmed <aliahmed@fedora>
2025-08-11 01:56:46 +0300

    Fix:script file

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 log_file="/home/aliahmed/logs/backup.log"
-backup_source="/etc"
+backup_source="/var/www"
 filename="backup-$(date +%F_%H-%M-%S).tar.gz"
 backup_dir="/home/aliahmed/backup"
 backup_dest="$backup_dir/$filename"
@@ -8,7 +8,10 @@ echo "Backup started at $(date)" >> "$log_file"
 tar -czf "$backup_dest" "$backup_source" >> "$log_file" 2>&1
 status=$?
 echo $status
-if [ $status -ne 0 ]; then
+if [ $status -eq 0 ]; then
+        echo "return to dir $backup_dir" for result | mail -s "backup succeeded" securevbn999@gmail.com
+else
         echo "return to $log_file to know the reason" | mail -s "backup failed" securevbn999@gmail.com
 fi
 find "$backup_dir" -name "*.tar.gz" -type f -mtime +7 -exec rm -f {} \;
+
